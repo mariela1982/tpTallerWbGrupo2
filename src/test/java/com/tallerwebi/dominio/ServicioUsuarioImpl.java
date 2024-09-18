717654c78799019df886efe04ac2ce29ba9f6ec6
mariela1982 <marimanri_24@hotmail.com>
2024-09-18 17:27:43 -0300

    limpiar

--- src/test/java/com/tallerwebi/dominio/ServicioUsuarioImpl.java
+++ src/test/java/com/tallerwebi/dominio/ServicioUsuarioImpl.java
@@ -1,51 +1,51 @@
-package com.tallerwebi.dominio;
+//package com.tallerwebi.dominio;
+//
+//
+//import com.tallerwebi.dominio.excepcion.PasswordLongitudIncorrecta;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import javax.transaction.Transactional;
+//
+//@Service
+//@Transactional
+//public class ServicioUsuarioImpl implements ServicioUsuario {
+//
+//
+//
+//
+//    RepositorioUsuario repositorioUsuario;
+//    //Inyeccion de dependencia
+//
+//
+//    @Autowired
+//    public ServicioUsuarioImpl(RepositorioUsuario repositorioUsuario) {
+//        this.repositorioUsuario = repositorioUsuario;
+//    }
+//
+//
 
-
-import com.tallerwebi.dominio.excepcion.PasswordLongitudIncorrecta;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
-
-@Service
-@Transactional
-public class ServicioUsuarioImpl implements ServicioUsuario {
-
-
-
-
-    RepositorioUsuario repositorioUsuario;
-    //Inyeccion de dependencia
-
-
-    @Autowired
-    public ServicioUsuarioImpl(RepositorioUsuario repositorioUsuario) {
-        this.repositorioUsuario = repositorioUsuario;
-    }
-
-
-
-    @Override
-    public Usuario registrar(String email, String password) throws PasswordLongitudIncorrecta {
-       if(password.length() < 5){
-           throw new PasswordLongitudIncorrecta();
-       }
-
-       Usuario usuarioBuscado = repositorioUsuario.buscar(email);
-
-       if(usuarioBuscado != null){
-           return null;
-       }
-
-
-       Usuario usuario = new Usuario();
-       usuario.setEmail(email);
-       usuario.setPassword(password);
-       repositorioUsuario.guardar(usuario);
-
-
-
-
-        return new Usuario();
-    }
-}
+//    @Override
+//    public Usuario registrar(String email, String password) throws PasswordLongitudIncorrecta {
+//       if(password.length() < 5){
+//           throw new PasswordLongitudIncorrecta();
+//       }
+//
+//       Usuario usuarioBuscado = repositorioUsuario.buscar(email);
+//
+//       if(usuarioBuscado != null){
+//           return null;
+//       }
+//
+//
+//       Usuario usuario = new Usuario();
+//       usuario.setEmail(email);
+//       usuario.setPassword(password);
+//       repositorioUsuario.guardar(usuario);
+//
+//
+//
+//
+//        return new Usuario();
+//    }
+//}
