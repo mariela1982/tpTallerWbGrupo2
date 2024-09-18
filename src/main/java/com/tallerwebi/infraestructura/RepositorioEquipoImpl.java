3f5e4f0d17db3e4d571ac5d97e41c2f8dbf83656
Kvnrot-sys <kvnrotela@gmail.com>
2024-09-18 02:55:16 -0300

    commit 1

---
+++ src/main/java/com/tallerwebi/infraestructura/RepositorioEquipoImpl.java
@@ -0,0 +1,55 @@
+package com.tallerwebi.infraestructura;
+
+
+import com.tallerwebi.dominio.Equipo;
+import com.tallerwebi.dominio.RepositorioEquipo;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+@Repository
+public class RepositorioEquipoImpl implements RepositorioEquipo {
+
+
+
+    SessionFactory sessionFactory;
+
+    @Autowired
+    public RepositorioEquipoImpl(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public Equipo buscar(String nombre) {
+
+
+        //conectarse a la base de datos
+        Session session = sessionFactory.getCurrentSession();
+
+
+        /* filtra si dentro de la base de datos encuentra al objeto con dicha
+        condicion (en este caso la de nombre)
+
+         */
+        return (Equipo)session.createCriteria(Equipo.class)
+                .add(Restrictions.eq("nombre",nombre))
+                .uniqueResult();
+    }
+
+    @Override
+    @Transactional
+    public void guardar(Equipo equipoCreado) {
+        Session session = sessionFactory.getCurrentSession();
+        session.save(equipoCreado);
+    }
+}
---
+++ src/test/java/com/tallerwebi/presentacion/ControladorEquipoTest.java
@@ -0,0 +1,58 @@
+package com.tallerwebi.presentacion;
+
+import com.tallerwebi.dominio.ControladorEquipo;
+import com.tallerwebi.dominio.ServicioEquipo;
+import com.tallerwebi.dominio.excepcion.EquipoExistente;
+import com.tallerwebi.infraestructura.ServicioEquipoImpl;
+import org.junit.Test;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalToIgnoringCase;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ControladorEquipoTest {
+
+    String nombreEquipo = "Los salchipapas";
+    Long dtDni = 45285663L;
+    String cbu = "4934607627390389151791";
+
+    ServicioEquipo servicioEquipo = mock(ServicioEquipoImpl.class);
+    ControladorEquipo controladorEquipo = new ControladorEquipo(servicioEquipo);
+
+
+
+    @Test
+    public void siExisteNombreDniYCbuElRegistroEsExitoso(){
+
+        ModelAndView mav = whenRegistroEquipo(nombreEquipo,cbu,dtDni);
+        thenRegistroEsExitoso(mav);
+
+    }
+
+    private void thenRegistroEsExitoso(ModelAndView mav) {
+        assertThat(mav.getViewName(),equalToIgnoringCase("redirect:/home"));
+    }
+
+    private ModelAndView whenRegistroEquipo(String nombreEquipo, String cbu, Long dtDni) {
+        return controladorEquipo.registrar(nombreEquipo,cbu,dtDni);
+    }
+
+
+    @Test
+    public void siExisteEquipoConNombreDelRegistroElRegistroFalla() throws EquipoExistente {
+
+        when(servicioEquipo.registrar(nombreEquipo,cbu,dtDni)).thenThrow(EquipoExistente.class);
+
+        ModelAndView mav = whenRegistroEquipo(nombreEquipo,cbu,dtDni);
+        thenRegistroFalla(mav,"El equipo ya existe");
+
+    }
+
+    private void thenRegistroFalla(ModelAndView mav, String mensaje) {
+        assertThat(mav.getViewName(),equalToIgnoringCase("formularioCreacionEquipo"));
+        assertThat(mav.getModel().get("error").toString(), equalToIgnoringCase(mensaje));
+
+    }
+}
