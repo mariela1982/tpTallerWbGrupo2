1d70ca4b406be10900238fc47dab44b37fa394d9
mariela1982 <marimanri_24@hotmail.com>
2024-10-04 16:33:59 -0300

    validarLogin

--- src/main/java/com/tallerwebi/presentacion/ControladorLogin.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorLogin.java
@@ -46,7 +46,7 @@ public class ControladorLogin {
 
         Usuario usuarioBuscado = servicioLogin.consultarUsuario(datosLogin.getEmail(), datosLogin.getPassword());
 
-        if (usuarioBuscado != null) {
+        if (usuarioBuscado != null&& !usuarioBuscado.getAdmin()) {
             HttpSession session = request.getSession(true);
             session.setAttribute("usuario", usuarioBuscado);
             return new ModelAndView("redirect:/directorTecnico");
